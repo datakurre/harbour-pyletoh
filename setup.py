882bb04415913f5a79988b46ef17b2dbabe11c0f
Asko Soukka <asko.soukka@iki.fi>
2015-02-22 11:06:09 +0200

    Prepare for a release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='harbour-pyletoh',
-    version='0.6.0',
+    version='0.6.1',
     author='Asko Soukka',
     author_email='asko.soukka@iki.fi',
     license='GPL',
