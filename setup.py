0a8cc6c696e1c4382bb127095d8eb8b20c828185
Asko Soukka <asko.soukka@iki.fi>
2015-09-24 20:57:11 +0300

    Prepare 0.6.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='harbour-pyletoh',
-    version='0.6.3',
+    version='0.6.4',
     author='Asko Soukka',
     author_email='asko.soukka@iki.fi',
     license='GPL',
